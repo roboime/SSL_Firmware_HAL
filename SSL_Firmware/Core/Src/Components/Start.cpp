a318b8e596ac987b19fc209a2164db12a0ec6921
Gabriel Lima <gabrielmartinslima@gmail.com>
2021-12-26 17:15:36 -0300

    Comment unused code

--- SSL_Firmware/Core/Src/Components/Start.cpp
+++ SSL_Firmware/Core/Src/Components/Start.cpp
@@ -17,7 +17,7 @@ extern TIM_HandleTypeDef htim6;
 extern void (*usbRecvCallback)(uint8_t*, uint32_t*);
 
 
-struct recvUSBStruct_t{
+/*struct recvUSBStruct_t{
 	uint32_t motorSpd_s;
 	int32_t motorSpd[4] = {0, 0, 0, 0};
 	int32_t dribbleSpd = 0;
@@ -25,7 +25,7 @@ struct recvUSBStruct_t{
 	uint32_t sevSeg;
 	uint32_t kickPow_s;
 	uint32_t kickPow[2];
-}*recvUSBStruct;
+}*recvUSBStruct;*/
 
 /*struct sendUSBStruct_t{
 	uint32_t motorEnc_s = 4;
@@ -39,10 +39,10 @@ sendUSBStruct_t sendUSBStruct = {
 	0
 };
 
-struct usbStruct_t{
+/*struct usbStruct_t{
 	int32_t val1;
 	int32_t val2;
-}usbStruct;
+}usbStruct;*/
 
 Encoder encoder(0);
 CommunicationUSB usb(&usbRecvCallback);
@@ -58,12 +58,12 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 void Start(){
 	Motor motor[4] = {Motor(0), Motor(1), Motor(2), Motor(3)};
 	while(1){
-		usbStruct.val1 = 100;
-		usbStruct.val2 = 101;
-		motor[0].SetDribble(recvUSBStruct->dribbleSpd);
+		/*usbStruct.val1 = 100;
+		usbStruct.val2 = 101;*/
+		/*motor[0].SetDribble(recvUSBStruct->dribbleSpd);
 		for(int i=0;i<4;i++){
 			motor[i].SetSpeed(recvUSBStruct->motorSpd[i]);
-		}
+		}*/
 		sendUSBStruct.button = HAL_GPIO_ReadPin(Btn_GPIO_Port, Btn_Pin);
 		//HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PinState(recvUSBStruct->led & 1));
 		//HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PinState(recvUSBStruct->led & 2));
