8d1e05d465c8cc4aeddb6e0baf2f9438d8b72379
Gabriel Lima <gabrielmartinslima@gmail.com>
2021-12-26 16:51:16 -0300

    Successful USB receive and decode

--- SSL_Firmware/Core/Src/Components/CommunicationUSB.cpp
+++ SSL_Firmware/Core/Src/Components/CommunicationUSB.cpp
@@ -10,10 +10,15 @@
 #include "usb_device.h"
 #include "usbd_cdc_if.h"
 #include "CommunicationUSB.hpp"
+#include "grSim_Commands.pb.h"
+#include "pb_decode.h"
+#include "pb_encode.h"
 
 extern sendUSBStruct_t sendUSBStruct;
 extern struct recvUSBStruct_t *recvUSBStruct;
 char package[24];
+grSim_Robot_Command receivedPacket = grSim_Robot_Command_init_default;
+
 
 /**
  * @param _usbRecvCallback pointer to global variable which is a function pointer to be called inside usbd_cdc_if.h
@@ -25,6 +30,8 @@ CommunicationUSB::CommunicationUSB(void (**_usbRecvCallback)(uint8_t*, uint32_t*
 
 void CommunicationUSB::ReceiveCallback(uint8_t* Buf, uint32_t* Len){
 	HAL_GPIO_TogglePin(LD4_GPIO_Port, LD4_Pin);
+	pb_istream_t stream = pb_istream_from_buffer(Buf, *Len);
+	pb_decode(&stream, grSim_Robot_Command_fields, &receivedPacket);
 }
 
 void CommunicationUSB::TransmitEncoderReadingRPM(int32_t reading){
