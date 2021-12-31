ab99ca7cfd65adc89fab21c63944bf767fc2c2d0
Gabriel Lima <gabrielmartinslima@gmail.com>
2021-12-31 18:05:49 -0300

    RF24 class

--- SSL_Firmware/Core/Src/Components/Start.cpp
+++ SSL_Firmware/Core/Src/Components/Start.cpp
@@ -12,8 +12,7 @@
 #include "Motor.hpp"
 #include "CommunicationUSB.hpp"
 #include "BTS7960B.hpp"
-
-#include "MY_NRF24.h"
+#include "RF24.hpp"
 
 extern TIM_HandleTypeDef htim6;
 extern UART_HandleTypeDef huart3;
@@ -27,6 +26,7 @@ uint8_t sendBuffer[64];
 Encoder encoder(0);
 CommunicationUSB usb(&usbRecvCallback);
 //BTS7960B motorbts(&(TIM10->CCR1), &(TIM11->CCR1), GPIOD, GPIO_PIN_0, GPIOD, GPIO_PIN_1);
+RF24 radio;
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
@@ -43,9 +43,9 @@ void packetReceivedCallback(void){
 
 void Start(){
 	//Motor motor[4] = {Motor(0), Motor(1), Motor(2), Motor(3)};
-	NRF24_begin(nRF_CSn_GPIO_Port, nRF_CSn_Pin, nRF_CE_GPIO_Port, nRF_CE_Pin, hspi1);
-	nrf24_DebugUART_Init(huart3);
-	printRadioSettings();
+	radio.begin(nRF_CSn_GPIO_Port, nRF_CSn_Pin, nRF_CE_GPIO_Port, nRF_CE_Pin, hspi1);
+	radio.DebugUART_Init(huart3);
+	radio.printRadioSettings();
 	while(1){
 		HAL_Delay(100);
 	}
